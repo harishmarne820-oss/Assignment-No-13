b95f30e1cc76413649af1d33eb63d1ef44522823
Harish <harishmarne820@gmail.com>
2026-07-10 22:25:16 +0530

    np.arange()

---
+++ Q3.py
@@ -0,0 +1,23 @@
+# Q3: np.arange()
+
+# Import NumPy library
+import numpy as np
+
+# a) Numbers from 0 to 20 (step 1)
+arr1 = np.arange(0, 21, 1)
+
+# b) Even numbers from 10 to 50
+arr2 = np.arange(10, 51, 2)
+
+# c) Numbers from 5 to 100 with step of 5
+arr3 = np.arange(5, 101, 5)
+
+# Print all arrays with labels
+print("a) Numbers from 0 to 20:")
+print(arr1)
+
+print("\nb) Even Numbers from 10 to 50:")
+print(arr2)
+
+print("\nc) Numbers from 5 to 100 with Step of 5:")
+print(arr3)
