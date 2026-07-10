9bef12d317bb5c678b74c601bd393e42689e295c
Harish <harishmarne820@gmail.com>
2026-07-10 21:40:01 +0530

    Added Q2: np.zeros() and np.ones()

---
+++ Q2.py
@@ -0,0 +1,23 @@
+# Q2: np.zeros() and np.ones()
+
+# Import NumPy library
+import numpy as np
+
+# a) Create a 1D array of 8 zeros
+arr1 = np.zeros(8)
+
+# b) Create a 2D array of shape (4,4) filled with ones
+arr2 = np.ones((4, 4))
+
+# c) Create a 3×3 matrix of zeros
+arr3 = np.zeros((3, 3))
+
+# Print all arrays
+print("1D Array of 8 Zeros:")
+print(arr1)
+
+print("\n2D Array (4x4) of Ones:")
+print(arr2)
+
+print("\n3x3 Matrix of Zeros:")
+print(arr3)
