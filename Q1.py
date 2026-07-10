c7590d5fd4f299890ae2db1644e9dd000ca02f1b
Harish <harishmarne820@gmail.com>
2026-07-10 21:45:11 +0530

    Basic Array Creation

--- Q1.py
+++ Q1.py
@@ -12,14 +12,14 @@ print(arr_1D)
 print("Shape:", arr_1D.shape)
 print("Data Type:", arr_1D.dtype)
 
-# Create a 2D array (3 × 3)
+# Create a 2D array (3×3)
 arr_2D = np.array([
     [10, 20, 30],
     [40, 50, 60],
     [70, 80, 90]
 ])
 
-# Print the 2D array
+# Print the 2D Array
 print("\n2D Array:")
 print(arr_2D)
 print("Shape:", arr_2D.shape)
